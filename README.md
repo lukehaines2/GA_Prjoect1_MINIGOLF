9c7a907d351316e49e6d5dca1f694ef62f00f370
LukeHaines <lukehaines2@gmail.com>
2015-08-26 23:18:24 +0100

    tidying up and indenting playing with buttons

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 
 *******************************************************
-                    **MINI GOLF**
+                    		**MINI GOLF**
 *******************************************************
 
-This is our first major project in the 3rd week of WDI.
+This is our first major project in the **3rd week of WDI**.
 <br>
 
-The technologies used are primarily:<br>
+The **technologies** used are primarily:<br>
 
 * HTML 5
 * CSS 3
@@ -14,11 +14,11 @@ The technologies used are primarily:<br>
 * jQuery (Library)
 * External libraries (fonts, additional functionality etc)
 
-My game is intended to be a basic version of the well known game of mini-golf.
+My game is intended to be a basic version of the well known game of **mini-golf**.
 
 The primary objective is to utilise as many of the skills accuired over the last 2 weeks as possible and to effectively implement them to create this game.
 
-Instructions:
+**Instructions:**
 
 Simple! Drag and rotate the ball in the direction you want it to travel, and let it go!
 
--- script.js
+++ script.js
@@ -3,7 +3,6 @@
                 // WDI 15 //
 //*********************************************//                
 
-
 $(document).ready(function(){
   console.log('ready');
 
@@ -11,128 +10,123 @@ $(document).ready(function(){
                 // FUNCTIONS //
 /*********************************************/
 
-//Displays the intro with slideup onclick
-$('#introScreen').ready(function(){
-    intro.delay(5000).slideUp('slow');
-});
+    //Displays the intro with slideup onclick
+    $('#introScreen').ready(function(){
+        intro.delay(5000).slideUp('slow');
+    });
 
-//CLICK COUNTER ON MOUSE-UP - called within ball
-function clickCounter() {
-    counter++;
-    $("#theCount").text('Shots : ' + counter);
-};
+    //CLICK COUNTER ON MOUSE-UP - called within ball
+    function clickCounter() {
 
-// FUNCTION: IF BALL HITS HOLE
-function isInTheHole(top, left) {
-    // need top 314 + width (13)
-    // left 530 + 13 (height)
-    console.log('top', top, 'left', left);
-    var topPos = (top >=300 && top <= 330);
-    var leftPos = (left >= 510 && left <= 553);
-    console.log(topPos, leftPos);
-    return topPos && leftPos;
-}
+        counter++;
+        $("#theCount").text('Shots : ' + counter);
+    };
 
-// EVENT LISTENERS: for Buttons
-// if ($('#powStrong').click(event1, event2)) {
-//     p = powStrong;
-// }
-// else if ($('#powWeak').click(event1, )
-
-
-/*********************************************/
-        //DECALRE THE GLOBAL VARIABLES//
-/*********************************************/
-var ball = $("#box"); //<--- Golf Ball
-var r;
-var counter = 0;
-var hole = $('#example');
-var intro = $('#introScreen');
-var powStrong = 250;
-var powMed = 150;
-var powWeak = 50;
-//COORDINATES DEFINED TO THE LEFT
-var y = $('#box').offset().top;
-var x = $('#box').offset().left;
-
-
-// GOLF BALL ROTATE FUNCTION
-function playItAll() {
-    
-    var RAD2DEG = 180 / Math.PI;            
-    //Rotate from the middle of the ball
-    ball.centerX = ball.offset().left + ball.width()/2;
-    ball.centerY =  ball.offset().top + ball.height()/2;
-
-    var offset, dragging=false;
-    ball.mousedown(function(e) {
-    dragging = true;
-    offset = Math.atan2(ball.centerY - e.pageY, e.pageX - ball.centerX);
-    console.log('mousedown');
-
-    // EVENT LISTENER: ADD ARROW TRAJECTORY GUIDE
-    $('#box').css('background-image', 'url(images/arrow2.png)');
-    })
-
-    // EVENT LISTENER FOR MOUSEUP
-    $(document).mouseup(function() {
-      dragging = false;
-      
-        // BALL TRAVEL - HECTIC MATHS!!
-        var dx = Math.cos(r) * 100/200; //<--
-        var dy = Math.sin(r) * 100/200; //<--0.5 per 10ms
-        // var xn = x0 + v * t * cos(theta)
+    // FUNCTION: IF BALL HITS HOLE
+    function isInTheHole(top, left) {
         
-        var orgX = x;
-        var orgY = y;
-        var movement = setInterval(function(){
-            $("#box").offset({top: y, left: x});
+        console.log('top', top, 'left', left);
+        var topPos = (top >=300 && top <= 330);
+        var leftPos = (left >= 510 && left <= 550);
+        console.log(topPos, leftPos);
+        return topPos && leftPos;
+    };
+
+    // EVENT LISTENERS: for Buttons
+    // if ($('#powStrong').click(event1, event2)) {
+    //     p = powStrong;
+    // }
+    // else if ($('#powWeak').click(event1, )
+
+
+    /*********************************************/
+            //DECALRE THE GLOBAL VARIABLES//
+    /*********************************************/
+    var ball = $("#box"); //<--- Golf Ball
+    var r;
+    var counter = 0;
+    var hole = $('#example');
+    var intro = $('#introScreen');
+    var powStrong = 250;
+    var powMed = 150;
+    var powWeak = 50;
+    //COORDINATES DEFINED TO THE LEFT
+    var y = $('#box').offset().top;
+    var x = $('#box').offset().left;
+
+
+    // GOLF BALL ROTATE FUNCTION
+    function playItAll() {
+        
+        // var RAD2DEG = 180 / Math.PI;            
+        //Rotate from the middle of the ball
+        ball.centerX = ball.offset().left + ball.width()/2;
+        ball.centerY =  ball.offset().top + ball.height()/2;
+
+        var offset, dragging=false;
+        ball.mousedown(function(e) {
+        dragging = true;
+        offset = Math.atan2(ball.centerY - e.pageY, e.pageX - ball.centerX);
+        console.log('mousedown');
+
+        // EVENT LISTENER: ADD ARROW TRAJECTORY GUIDE
+        $('#box').css('background-image', 'url(images/arrow2.png)');
+        })
+
+        // EVENT LISTENER FOR MOUSEUP
+        $(document).mouseup(function() {
+          dragging = false;
+          
+            // BALL TRAVEL - HECTIC MATHS!!
+            var dx = Math.cos(r) * 100/200; //<--
+            var dy = Math.sin(r) * 100/200; //<--0.5 per 10ms
+            // var xn = x0 + v * t * cos(theta)
             
-            x += dx;
-            y += dy;
+            var orgX = x;
+            var orgY = y;
+            var movement = setInterval(function() {
+                $("#box").offset({top: y, left: x});
+                
+                x += dx;
+                y += dy;
 
-            var winning = isInTheHole(y,x);
-            console.log('win?', !!winning)
-            if (!!winning) {
-                alert('YOU WON MATE!!')
-                clearInterval(movement);
-            }
+                var winning = isInTheHole(y,x);
+                console.log('win?', !!winning)
+                if (!!winning) {
+                    alert('YOU WON MATE!!')
+                    clearInterval(movement);
+                }
 
-            if (Math.cos(r) !== 0) {
-                if ( Math.abs((x - orgX) * Math.cos(r)) > 200 ){
-                clearInterval(movement);
-                } 
-            }
-            else if (Math.abs(y - orgY) > 200) {
-                clearInterval(movement);
-            }
-        }, 1)
+                if (Math.cos(r) !== 0) {
+                    if ( Math.abs((x - orgX) * Math.cos(r)) > 200 ) {
+                    clearInterval(movement);
+                    } 
+                }
+                else if (Math.abs(y - orgY) > 200) {
+                    clearInterval(movement);
+                }
+            }, 1)
 
-        // Change image back to regular ball
-        $('#box').css('background-image', 'url(images/golf-ball.png)');
-        // Call counter function on mouseUp
-        clickCounter()
+            // Change image back to regular ball
+            $('#box').css('background-image', 'url(images/golf-ball.png)');
+            
+            clickCounter() // <--Call counter function on mouseUp
 
-    })  // <---end of mouseUp
-    
-    // EVENT LISTENER: DIRECTION OF ROTATE
-    $(document).mousemove(function(e) {
-          if (dragging) { 
-            // Recalculating the end position after drag
-            var newOffset = Math.atan2(ball.centerY - e.pageY, e.pageX - ball.centerX);
-            // r = Prjected angle
-            r = (offset - newOffset); // <---Deleted Degree Conversion from here
+        })  // <---End of mouseUp
+        
+        // EVENT LISTENER: DIRECTION OF ROTATE
+        $(document).mousemove(function(e) {
+              if (dragging) { 
+                // Recalculating the end position after drag
+                var newOffset = Math.atan2(ball.centerY - e.pageY, e.pageX - ball.centerX);
+                // r = Projected angle
+                r = (offset - newOffset); // <---Deleted Degree Conversion from here
 
-            ball.css('-webkit-transform', 'rotate(' + r + 'rad)');      
-          }
-    })
-};
+                ball.css('-webkit-transform', 'rotate(' + r + 'rad)');      
+              }
+        })
+    };
 
-playItAll();
-
-
-
-
-
+    playItAll();
 
 }); // <--- Closing the Doc Ready
